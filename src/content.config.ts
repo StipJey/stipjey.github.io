3187086452cda365386987046da1d39f313d4932
Stipjey <StipJey@gmail.com>
2026-02-01 18:41:38 +0300

    Сделал страницу с промокодами и рефералками

--- src/content.config.ts
+++ src/content.config.ts
@@ -15,4 +15,22 @@ const blog = defineCollection({
 	}),
 });
 
-export const collections = { blog };
+const recommendations = defineCollection({
+	loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/recommendations" }),
+	schema: z.object({
+		title: z.string(),
+		description: z.string().optional(),
+		items: z.array(z.object({
+			title: z.string(),
+			benefit: z.string(),
+			steps: z.array(z.string()),
+			recommendation: z.string(),
+			type: z.enum(['promo', 'link', 'other']),
+			code: z.string().optional(),
+			link: z.string().optional(),
+			linkText: z.string().optional(),
+		}))
+	})
+});
+
+export const collections = { blog, recommendations };
