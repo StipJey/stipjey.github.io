3cb5848ceb99d378433ca7b3740396d0d592c5b5
Stipjey <StipJey@gmail.com>
2026-03-01 13:52:26 +0300

    Проставил правильный язык

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+// Глобальные данные сайта.
+// Можно импортировать из любого файла проекта.
 
 export const SITE_TITLE = `Евгений Черкасов`;
 export const SITE_DESCRIPTION = 'Senior Frontend Developer and Team Leader!';
